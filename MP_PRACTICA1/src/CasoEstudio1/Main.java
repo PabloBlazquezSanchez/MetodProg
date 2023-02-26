d65fb8b1142178a3f77ccdc833c0284171802029
Andrea <andreaop01@gmail.com>
2023-02-26 13:33:19 +0100

    tiempoEjecucion
    Añadido el metodo de tiempo de ejecucion para las tres formulas

--- MP_PRACTICA1/src/CasoEstudio1/Main.java
+++ MP_PRACTICA1/src/CasoEstudio1/Main.java
@@ -3,14 +3,17 @@ package CasoEstudio1;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class main {
 	public static void main(String[] args) {
 		System.out.println("Introduce el valor para calcular el n-ésimo número hexagonal:");
 		int numero = filtrarNumero();
-
+		long tiempoMetodo1 = Metodos.tiempoEjecucionMetodo1(numero);
+		long tiempoMetodo2 = Metodos.tiempoEjecucionMetodo2(numero);
+		long tiempoMetodo3 = Metodos.tiempoEjecucionMetodo3(numero);
+		System.out.println("El tiempo de ejecución de la formula cerrada es: " +tiempoMetodo1+"\nEl tiempo de ejecución de la formula iterativa es: " +tiempoMetodo2+"\nEl tiempo de ejecución de la formula recursiva es: "+tiempoMetodo3);
 	}
 
-	/**
+	 /**
 	 * 
 	 * Este método captura el dato introducido por teclado y analiza si se ha
 	 * introducido un caracter o un número. Si se introduce un caracter lanza un
@@ -18,12 +21,13 @@ public class Main {
 	 * Mismatch Exception".
 	 * 
 	 * @author Pablo Blázquez Sánchez, Jesús Fernández López, Raúl Jiménez de la
-	 *         Cruz
+	 *         Cruz, Andrea Ordoño Peña
 	 * @version 1.0
 	 *
 	 * @return numero El número escrito por el usuario
 	 */
-
+	
+	
 	public static int filtrarNumero() {
 		int numero = 0;
 		Scanner lectura = new Scanner(System.in);
@@ -48,7 +52,7 @@ public class Main {
 	}
 }
 
-//Clase: Main
+//Clase: MainPuebas
 //Metodo: pruebaEjemplo
 //Variable: persona
 
@@ -56,3 +60,5 @@ public class Main {
 //Con 0 da mal el resultado
 // Controlar los negativos
 // Da error 99999
+
+// Comprobar resultado de tiempo ejecucion con ejercicios resueltos
