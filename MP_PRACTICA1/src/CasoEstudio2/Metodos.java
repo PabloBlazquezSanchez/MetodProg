73914adf523c63e48b178e10b6c5a9047fc057a8
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-03-10 11:42:00 +0100

    jj

--- MP_PRACTICA1/src/CasoEstudio2/Metodos.java
+++ MP_PRACTICA1/src/CasoEstudio2/Metodos.java
@@ -1,5 +1,17 @@
 package CasoEstudio2;
 
 public class Metodos {
+	public static int countInversions(int[] a, int left, int right) {
+		int count = 0;
 
+		return count;
+	}
+
+	public static int mergeAndCount(int[] a, int left, int middle, int right) {
+		//Maintain a pointer into each subarray, pointing to the front elements
+
+		int count = 0; //Maintain a variable Count for the number of inversions, initialized to 0
+
+		return count;
+	}
 }
