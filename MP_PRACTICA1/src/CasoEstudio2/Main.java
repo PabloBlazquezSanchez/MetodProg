306abe12199f91cf6a8d30cdfa34455773f13d95
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-03-16 18:02:38 +0100

    kkk

--- MP_PRACTICA1/src/CasoEstudio2/Main.java
+++ MP_PRACTICA1/src/CasoEstudio2/Main.java
@@ -1,23 +1,46 @@
 package CasoEstudio2;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Main.
  */
 public class Main {
-	
+	static double[] b;
+
 	/**
 	 * The main method.
 	 *
 	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
-		
-		
-//System.out.println(Metodos.countInversions(a, left, right));
+		openFile();
+		System.out.println(Metodos.countInversions(b, 0, b.length-1));
+
+	}
+
+	/**
+	 * Open file.
+	 */
+	public static void openFile() {
+		try {
+			Scanner read = new Scanner(new File("InversionsTest_6.dat"));
+			int nCases = read.nextInt(); // El primer valor es el número de casos
+			double[] a = new double[nCases];
+			for (int i = 0; i < nCases; i++) {
+				a[i] = read.nextInt();
+			}
+			b = a;
+		} catch (FileNotFoundException e) { // Si el fichero a buscar no existe lanza este mensaje de error y finaliza
+											// el programa
+			System.out.println("El fichero no existe en el directorio de búsqueda.\n"
+					+ "El programa no puede continuar ejecutándose. Fin del Programa.");
+			System.exit(0); // Finaliza el programa
+		} catch (IOException e) { // Detecta otros errores
+			e.printStackTrace();
+		}
 	}
 }
--- MP_PRACTICA1/src/CasoEstudio2/Metodos.java
+++ MP_PRACTICA1/src/CasoEstudio2/Metodos.java
@@ -5,12 +5,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Metodos.
  */
+
+
 public class Metodos {
-	
+		
 	/**
 	 * Count inversions.
 	 *
@@ -19,7 +20,7 @@ public class Metodos {
 	 * @param right the right
 	 * @return the int
 	 */
-	public static int countInversions(int[] a, int left, int right) {
+	public static int countInversions(double [] a, int left, int right) {
 		int middle = (left + right) / 2; // Floor operator
 		int count = 0;
 
@@ -32,47 +33,7 @@ public class Metodos {
 		return count;
 	}
 
-//	public static void merge (int [ ] a, int left, int middle, int right) {
-//		int i = left, j = middle+1, k = left;
-//		int [] aux = new int[right-left+1]; // Creamos el array auxiliar
-//		for (int f = left; f <= right; f++) // copiamos las dos mitades del array en el array auxiliar
-//			aux[f-left] = a[f];
-//		while (i <= middle && j <= right) {  //copia el siguiente elemento que sea más grande
-//			if (aux[i-left] <= aux[j-left]) {
-//				a[k] = aux[i-left];
-//				i++;
-//			} else {
-//				a[k] = aux[j-left];
-//				j++;
-//			}
-//			k++;
-//		}
-//		if (j-1 == right) {  // copia lo que queda del array de la primera mitad
-//			while (i<= middle) {
-//				a[k] = aux[i-left];
-//				k++;
-//				i++;
-//			}
-//		}
-//	}
-
-	/**
- * Mergesort.
- *
- * @param array the array
- * @param left the left
- * @param right the right
- */
-public static void mergesort(int[] array, int left, int right) { //faltaba este metodo
-		if (left < right) {
-			int mid = (int) Math.floor((left + right) / 2); //me aseguro de que se trunque / función suelo
-			//int mid = (left + right) / 2;
-			mergesort(array, left, mid);
-			mergesort(array, mid + 1, right);
-			mergeAndCount(array, left, mid, right);
-		}
-	}
-
+	
 	/**
 	 * Merge and count.
 	 *
@@ -82,10 +43,11 @@ public static void mergesort(int[] array, int left, int right) { //faltaba este 
 	 * @param right the right
 	 * @return the int
 	 */
-	public static int mergeAndCount(int[] a, int left, int middle, int right) {
+	
+	public static int mergeAndCount(double[] a, int left, int middle, int right) {
 		int count = 0; // Maintain a variable Count for the number of inversions, initialized to 0
 		int i = left, j = middle + 1, k = left; // Maintain a pointer into each subarray, pointing to the front elements
-		int[] current = new int[right - left + 1]; // Creamos el array auxiliar
+		double [] current = new double[right - left + 1]; // Creamos el array auxiliar
 		for (int f = left; f <= right; f++) // copiamos las dos mitades del array en el array auxiliar
 			current[f - left] = a[f];
 		while (i <= middle && j <= right) { // copia el siguiente elemento que sea más grande
@@ -110,24 +72,4 @@ public static void mergesort(int[] array, int left, int right) { //faltaba este 
 		return count;
 	}
 
-	/**
-	 * Open file.
-	 */
-	public static void openFile() {
-		try {
-			Scanner read = new Scanner(new File("InversionsTest_6.dat"));
-			int nCases = read.nextInt(); // El primer valor es el número de casos
-			double[] a = new double[nCases];
-			for (int i = 0; i < nCases; i++) {
-				a[i] = read.nextInt();
-			}
-		} catch (FileNotFoundException e) { // Si el fichero a buscar no existe lanza este mensaje de error y finaliza
-											// el programa
-			System.out.println("El fichero no existe en el directorio de búsqueda.\n"
-					+ "El programa no puede continuar ejecutándose. Fin del Programa.");
-			System.exit(0); // Finaliza el programa
-		} catch (IOException e) { // Detecta otros errores
-			e.printStackTrace();
-		}
-	}
 }
