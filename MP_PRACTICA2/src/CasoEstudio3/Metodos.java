70169275998922363f5d48dd0970206069852b1d
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2023-04-13 00:40:18 +0200

    fg

--- MP_PRACTICA2/src/CasoEstudio3/Metodos.java
+++ MP_PRACTICA2/src/CasoEstudio3/Metodos.java
@@ -2,6 +2,7 @@ package CasoEstudio3;
 
 import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Metodos {
@@ -39,9 +40,56 @@ public class Metodos {
 		}
 		return tamanosBaldosas;
 	}
+	
+	/*
+	 * Algorithm tileFloor (tiles, n)
+	 * current = 0
+	 * set empty solution
+	 * sort tiles in decreasing order
+	 * while there is room for whole tiles do
+	 * if room for size tile(current) then
+	 * 		place tile of size tile(current)
+	 * 		reduce available floor surface
+	 * 		add one tile of size tile(current) to solution
+	 * else
+	 * 		current = current +1
+	 * end_if
+	 * 
+	 * end_while
+	 * return solution
+	 */
 
 	public static int[][] resolverColocacion(int tamSolar, int[] tamBaldosas) {
+		int current = 0;
+		int lado = tamSolar;
+		int actual = 0;
+		int tamano = tamBaldosas[actual];
 		int resultado[][] = new int[tamSolar][tamSolar];
+		for (int i = 0; i < resultado.length; i++) {
+			for (int j = 0; j < resultado[i].length; j++) {
+				while (resultado[i][j] == 0 && actual <= tamBaldosas.length - 1) {
+					tamano = tamBaldosas[actual];
+					if (cabeBaldosa(tamano, i, j, resultado, lado)) {
+						for (int f = i; f < i + tamano; f++) {
+							for (int c = j; c < j + tamano; c++) {
+								resultado[f][c] = tamano;
+							}
+						}
+						//puestas[actual]++;
+					}else {
+						actual++;
+					}
+				}
+			}
+		}
+		while (current < resultado.length - 1) {
+
+		}
 		return resultado;
+	}
+
+	private static boolean cabeBaldosa(int tamano, int i, int j, int[][] resultado, int lado) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
