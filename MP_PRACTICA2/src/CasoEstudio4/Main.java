ba68b46ef633395655b251c8a84560c3274fc1d7
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-04-20 19:23:38 +0200

    kk

--- MP_PRACTICA2/src/CasoEstudio4/Main.java
+++ MP_PRACTICA2/src/CasoEstudio4/Main.java
@@ -1,35 +1,40 @@
 package CasoEstudio4;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		leerFichero();
+		Beca becas[] = leerFichero("Fellowships3400.dat");
+		System.out.println("d");
 	}
 
-	public static void leerFichero() {
-		try {
-			Scanner read = new Scanner(new File("beca.dat"));
-			int nFilas = read.nextInt();// El primer valor es el número de filas totales que hay en el fichero
-			Beca becas[] = new Beca[nFilas];
-			for (int i = 0; i < nFilas; i++) {
-				String estructura = read.nextLine();
-				String[] data = estructura.split("\t");
-				becas[i].setId(Integer.parseInt(data[0]));
-				becas[i].setfInicio(Integer.parseInt(data[1]));
-				becas[i].setfFin(Integer.parseInt(data[2]));
-				becas[i].setDinero(Integer.parseInt(data[3]));
+	public static Beca[] leerFichero(String fichero) {
+		try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+			int longitud = Integer.parseInt(br.readLine());
+			Beca becas[] = new Beca[longitud];
+			for (int i = 0; i < longitud; i++) {
+				String[] data = br.readLine().split("\t");
+				int id = Integer.parseInt(data[0]);
+				int inicio = Integer.parseInt(data[1]);
+				int fin = Integer.parseInt(data[2]);
+				int dinero = Integer.parseInt(data[3]);
+				becas[i] = new Beca(id, inicio, fin, dinero); // inicializar la beca en la posición i del array
 			}
-		} catch (FileNotFoundException e) { // Si el fichero a buscar no existe lanza este mensaje de error y finaliza
-											// el programa
+			return becas;
+
+		} catch (FileNotFoundException e) { // Si el fichero a buscar no existe lanza este mensaje de error y finaliza el programa
 			System.out.println("El fichero no existe en el directorio de búsqueda.\n"
 					+ "El programa no puede continuar ejecutándose. Fin del Programa.");
 			System.exit(0); // Finaliza el programa
 		} catch (@SuppressWarnings("hiding") IOException e) { // Detecta otros errores
 			e.printStackTrace();
 		}
+		return null;
 	}
+
 }
