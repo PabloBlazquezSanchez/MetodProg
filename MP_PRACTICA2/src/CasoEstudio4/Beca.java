7aa7d34461391d723e08ee965a71dccf4fffd3d1
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-04-20 22:48:32 +0200

    jj

--- MP_PRACTICA2/src/CasoEstudio4/Beca.java
+++ MP_PRACTICA2/src/CasoEstudio4/Beca.java
@@ -1,47 +1,105 @@
 package CasoEstudio4;
 
+/**
+ * The Class Beca.
+ */
 public class Beca {
+	
+	/** The inicio. */
 	private int fInicio;
 
+	/** The fin. */
 	private int fFin;
+	
+	/** The dinero. */
 	private int dinero;
+	
+	/** The id. */
 	private int id;
 
-	public Beca(int fInicio, int fFin, int dinero, int id) {
+	/**
+	 * Instantiates a new beca.
+	 *
+	 * @param id the id
+	 * @param fInicio the f inicio
+	 * @param fFin the f fin
+	 * @param dinero the dinero
+	 */
+	public Beca(int id, int fInicio, int fFin, int dinero) {
 		this.fInicio = fInicio;
 		this.fFin = fFin;
 		this.dinero = dinero;
 		this.id = id;
 	}
 
+	/**
+	 * Gets the f inicio.
+	 *
+	 * @return the f inicio
+	 */
 	public int getfInicio() {
 		return fInicio;
 	}
 
+	/**
+	 * Sets the f inicio.
+	 *
+	 * @param fInicio the new f inicio
+	 */
 	public void setfInicio(int fInicio) {
 		this.fInicio = fInicio;
 	}
 
+	/**
+	 * Gets the f fin.
+	 *
+	 * @return the f fin
+	 */
 	public int getfFin() {
 		return fFin;
 	}
 
+	/**
+	 * Sets the f fin.
+	 *
+	 * @param fFin the new f fin
+	 */
 	public void setfFin(int fFin) {
 		this.fFin = fFin;
 	}
 
+	/**
+	 * Gets the dinero.
+	 *
+	 * @return the dinero
+	 */
 	public int getDinero() {
 		return dinero;
 	}
 
+	/**
+	 * Sets the dinero.
+	 *
+	 * @param dinero the new dinero
+	 */
 	public void setDinero(int dinero) {
 		this.dinero = dinero;
 	}
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
--- MP_PRACTICA2/src/CasoEstudio4/Main.java
+++ MP_PRACTICA2/src/CasoEstudio4/Main.java
@@ -1,40 +1,25 @@
 package CasoEstudio4;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-
+/**
+ * The Class Jes√∫s.
+ */
 public class Main {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
-		Beca becas[] = leerFichero("Fellowships3400.dat");
-		System.out.println("d");
-	}
-
-	public static Beca[] leerFichero(String fichero) {
-		try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
-			int longitud = Integer.parseInt(br.readLine());
-			Beca becas[] = new Beca[longitud];
-			for (int i = 0; i < longitud; i++) {
-				String[] data = br.readLine().split("\t");
-				int id = Integer.parseInt(data[0]);
-				int inicio = Integer.parseInt(data[1]);
-				int fin = Integer.parseInt(data[2]);
-				int dinero = Integer.parseInt(data[3]);
-				becas[i] = new Beca(id, inicio, fin, dinero); // inicializar la beca en la posiciÛn i del array
+		int[] bec = Metodos.CalcularMejorSolucion();
+		Beca[] becas = Metodos.leerFichero();
+		int totalDinero = 0;
+		for (int i = 0; i < bec.length; i++) {
+			if (bec[i] == 1) {
+				totalDinero += becas[i].getDinero();
+				System.out.println("Beca " + becas[i].getId() + ": ‚Ç¨" + becas[i].getDinero());
 			}
-			return becas;
-
-		} catch (FileNotFoundException e) { // Si el fichero a buscar no existe lanza este mensaje de error y finaliza el programa
-			System.out.println("El fichero no existe en el directorio de b˙squeda.\n"
-					+ "El programa no puede continuar ejecut·ndose. Fin del Programa.");
-			System.exit(0); // Finaliza el programa
-		} catch (@SuppressWarnings("hiding") IOException e) { // Detecta otros errores
-			e.printStackTrace();
 		}
-		return null;
+		System.out.println("Total dinero obtenido: ‚Ç¨" + totalDinero);
 	}
-
 }
