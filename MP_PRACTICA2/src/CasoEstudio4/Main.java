936da40d5f9c51dcdd3d4679763d56b6ff9f8ab6
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-04-22 00:05:25 +0200

    wwqw

--- MP_PRACTICA2/src/CasoEstudio4/Main.java
+++ MP_PRACTICA2/src/CasoEstudio4/Main.java
@@ -1,31 +1,41 @@
 package CasoEstudio4;
 
 /**
- * Es la clase principal del programa encargada de interactuar con el usuario, es decir, en este caso vamos a mostrar la 
- * mejor opción de becas a solicitar durante un curso. Para ello vamos a usar los métodos mejorSolucion y el método LeerFichero 
- * y la imprime en la consola. Encaso de que no sea posible mostrar la mejor opción de becas, la clase principal mostrará un 
- * mensaje de que no hay una opción adecuada con los criterios establecidos.
+ * Es la clase principal del programa encargada de interactuar con el usuario,
+ * es decir, en este caso vamos a mostrar la mejor opción de becas a solicitar
+ * durante un curso. Para ello vamos a usar los métodos mejorSolucion y el
+ * método LeerFichero y la imprime en la consola. En caso de que no sea posible
+ * mostrar la mejor opción de becas, la clase principal mostrará un mensaje de
+ * que no hay una opción adecuada con los criterios establecidos.
  */
 public class Main {
 
 	/**
+	 * Este es el método principal de la clase donde se realiza la interacción con
+	 * el usuario. En el método main, creamos un array bec y otro array de tipo Beca
+	 * llamado becas.
 	 * 
-	 * En el método main, creamos un array bec y otro array de tipo Beca llamado becas.
+	 * A bec le pasamos el método de mejor solución de la clase Metodos y a becas el
+	 * método de Leerfichero de la clase Metodos que contine todos los datos a usar.
+	 * Inicializamos la variable totalDinero de tipo entero en el cual vamos
+	 * almacenar el dinero total.
 	 * 
-	 * A bec le pasamos el método de mejor solución de la clase Metodos y a becas el método de Leerfichero de la clase Metodos 
-	 * que contine todos los datos a usar. Inicializamos la variable totalDinero de tipo entero en el cual vamos almacenar el 
-	 * dinero total.
+	 * Creamos un bucle if que va a tener el if y el else; en el if si la longitud
+	 * de bec es distinto de 0 vamos a imprimir un mensaje que diga la selección de
+	 * las becas, y crear un bucle for interno en el cual, vamos a recorrer el array
+	 * bec y añadimos otro bucle if interno que si cada posicion es igual a 1
+	 * entonces, a totalDinero le sumamos lo que tiene + el contenido del array
+	 * becas en esa posicion el dinero y lo imprimos cerramos todos los bucles
+	 * excepto el primer if y ahí se imprime el dinero total que hemos obtenido con
+	 * esto acabaríamos la primera opción.
 	 * 
-	 * Creamos un bucle if que va a tener el if y el else; en el if si la longitud de bec es distinto de 0 vamos a imprimir un 
-	 * mensaje que diga la seleccion de las becas, y crear un bucle for interno en el cual, vamos a recorrer el array bec y 
-	 * añadimos otro bucle if interno que si cacda posicion es igual a 1 entonces, a totalDinero le sumamos lo que tiene + el 
-	 * contenido del array becas en esa posicion el dinero y lo imprimos cerramos todos los bucles excepto el primer if y ahi 
-	 * imprimos el dinero total que hemos obtenido con esto acabariamos la primera opcion.
+	 * En el else del primer if que tenemos, si es igual 0 en ese caso solo
+	 * imprimiremos un mensaje que diga que no se ha considerado seleccionar ninguna
+	 * beca que se encuentra ofertada.
 	 * 
-	 * En el else del primer if que tenemos, si es igual 0 en ese caso solo imprimiremos un mensaje que diga que no se ha 
-	 * considerado seleccionar ninguna beca que se encuentra ofertada. 
 	 * 
-	 * @param args the arguments
+	 * 
+	 * @param args Argumentos del programa
 	 */
 	public static void main(String[] args) {
 		int[] bec = Metodos.mejorSolucion();
--- MP_PRACTICA2/src/CasoEstudio4/Metodos.java
+++ MP_PRACTICA2/src/CasoEstudio4/Metodos.java
@@ -6,24 +6,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * Esta clase contiene los métodos necesarios para la implementación del algoritmo de backtracking que nos permita 
- * resolver el problema. 
+ * Esta clase contiene los métodos necesarios para la implementación del
+ * algoritmo de backtracking que nos permita resolver el problema.
  */
 public class Metodos {
 
 	/**
 	 * El objetivo de este método es buscar la mejor solución que existe.
 	 * 
-	 * Para calcular mejorSolucion, creamos una método estático array de tipo entero, lo primero que vamos hacer es 
-	 * crear un array becas de tipo Beca en el cual le pasamos el método de LeerFichero, creamos otro array solucion 
-	 * de tipo entero el cual esta inicializado con la longitud del array becas.
+	 * Para calcular mejorSolucion, creamos una método estático array de tipo
+	 * entero, lo primero que vamos hacer es crear un array becas de tipo Beca en el
+	 * cual le pasamos el método de LeerFichero, creamos otro array solucion de tipo
+	 * entero el cual esta inicializado con la longitud del array becas.
 	 * 
-	 * Llamamos al método de seleccion a la cual le pasamos la longitud del array, el array solucion, el nivel y 
-	 * el array becas. 
+	 * Llamamos al método de selección al cuál le pasamos la longitud del array, el
+	 * array solucion, el nivel y el array becas.Para finalizar el método retornamos
+	 * el array solucion.
 	 * 
-	 * Y para finalizar el método retornamos el array solucion.
-	 * 
-	 * @return devolvos el array int [] solucion
+	 * @return solucion Array de unos y ceros que representan la solución al
+	 *         problema
 	 */
 	public static int[] mejorSolucion() {
 		Beca becas[] = leerFichero();
@@ -34,10 +35,10 @@ public class Metodos {
 
 	/**
 	 *
-	 * @param x de x
+	 * @param x        de x
 	 * @param solucion de solucion
-	 * @param nivel de nivel
-	 * @param becas de becas
+	 * @param nivel    de nivel
+	 * @param becas    de becas
 	 */
 	public static void seleccion(int[] x, int[] solucion, int level, Beca[] becas) {
 		if (level == becas.length) {
@@ -94,26 +95,33 @@ public class Metodos {
 
 	/**
 	 * 
-	 * El objetivo de este método es saber si becaProv es compatible con las becas que se encuentran en el array becas.
+	 * El objetivo de este método es saber si becaProv es compatible con las becas
+	 * que se encuentran en el array becas.
 	 * 
-	 * Para ello vamos a crear un método estactico esCompatible de tipo booleano en el cual le pasamos la variable 
-	 * becaProv de tipo Beca, un array x tipo entero, una variable level de tipo entero y un array becas de tipo Beca.
+	 * Para ello vamos a crear un método estático esCompatible de tipo booleano en
+	 * el cual le pasamos la variable becaProv de tipo Beca, un array x tipo entero,
+	 * una variable level de tipo entero y un array becas de tipo Beca.
 	 * 
-	 * Dentro del método vamos a crear una variable comp de tipo booleano inicializado a true, como podemos ver después 
-	 * cremos un bucle for en el que vamos a recorrer todos los elementos del array x hasta el indice level. Dentro del bucle 
-	 * vamos a crear otro bucle interno pero de tipo if en el cual vamos a comparar todas las posiciones del array x con 1,
-	 * es decir,  si la posicion que actualmente se esta comparando  es igual a 1 se selecciona esa beca y si es asi, se 
-	 * obtiene el objeto Beca correspondiente en el array becas y se comprueba si hay algún solapamiento en las fechas 
-	 * entre becaProv y becaInd.
+	 * Dentro del método vamos a crear una variable comp de tipo booleano
+	 * inicializada a true, como podemos ver después creamos un bucle for en el que
+	 * vamos a recorrer todos los elementos del array x hasta el índice level.
+	 * Dentro del bucle vamos a crear otro bucle interno pero de tipo if en el cual
+	 * vamos a comparar todas las posiciones del array x con 1, es decir, si la
+	 * posicion que actualmente se esta comparando es igual a 1 se selecciona esa
+	 * beca y si es asi, se obtiene el objeto Beca correspondiente en el array becas
+	 * y se comprueba si hay algún solapamiento en las fechas entre becaProv y
+	 * becaInd.
 	 * 
-	 * Si se encuentra solapamiento la variable comp se estable a false y se sale del bucle. Si no se encuentra ningún 
-	 * solapamiento se sigue interando. Finalmente, el método retorna el valor de comp.
+	 * Si se encuentra solapamiento la variable comp se estable a false y se sale
+	 * del bucle. Si no se encuentra ningún solapamiento se sigue iterando.
+	 * Finalmente, el método retorna el valor de comp.
 	 * 
-	 * @param becaProv de beca provisional
-	 * @param x de x
-	 * @param nivel de nivel
-	 * @param becas de  ficehro fellowships
-	 * @return verdadero, si es compatible
+	 * 
+	 * @param becaProv Beca a la que se está evaluando si es compatible
+	 * @param x        Solución provisional al problema
+	 * @param nivel    Indica el nivel de profundidad del arbol de Backtracking
+	 * @param becas    Array con las becas almacenadas en el archivo .dat
+	 * @return verdadero Si la solución nueva es compatible
 	 */
 	public static boolean esCompatible(Beca becaProv, int[] x, int level, Beca[] becas) {
 		boolean comp = true;
@@ -131,24 +139,31 @@ public class Metodos {
 	/**
 	 * El objetivo de este método es la mejor beca que puede optar.
 	 * 
-	 * Para ello vamos a crear un método estático esMejor de tipo booleano, en el cual le vamos a pasar tres arrays, el primero 
-	 * es un array x de tipo entero, el segundo es un array solucion de tipo entero y por último un array becas de tipo Beca.  
+	 * Para ello vamos a crear un método estático esMejor de tipo booleano, en el
+	 * cual le vamos a pasar tres arrays, el primero es un array x de tipo entero,
+	 * el segundo es un array solución de tipo entero y por último un array becas de
+	 * tipo Beca.
 	 * 
-	 * Dentro del método vamos a crear variables sx y ss de tipo entero inicializado a 0, creamos un bucle for con el cual vamos 
-	 * a recorrer el array x, dentro creamos otro bucle de tipo if en el en cual vamos a comparar las posiciones del array x con 
-	 * 1, es decir, si la posicion que actualmente se esta comparando  es igual a 1 se almacena en sx y le sumamos lo que tiene + 
-	 * lo que hay en el array becas en esas posiciones y cerramos el bucle if.
+	 * Dentro del método vamos a crear variables sx y ss de tipo entero inicializado
+	 * a 0, creamos un bucle for con el cual vamos a recorrer el array x, dentro
+	 * creamos otro bucle de tipo if en el en cual vamos a comparar las posiciones
+	 * del array x con 1, es decir, si la posición que actualmente se esta
+	 * comparando es igual a 1 se almacena en sx y le sumamos lo que tiene más lo
+	 * que hay en el array becas en esas posiciones y cerramos el bucle if.
 	 * 
-	 * Abrimos el bucle if en el en cual vamos a comparar las posiciones del array soluciones con 1, es decir, si la posicion que
-	 * actualmente se esta comparando  es igual a 1 se almacena en ss y le sumamos lo que tiene +  lo que hay en el array becas en
-	 * esas posiciones y cerramos el bucle if.
+	 * Abrimos el bucle if en el en cual vamos a comparar las posiciones del array
+	 * soluciones con 1, es decir, si la posición que actualmente se está comparando
+	 * es igual a 1 se almacena en ss y le sumamos lo que tiene más lo que hay en el
+	 * array becas en esas posiciones y cerramos el bucle if.
 	 * 
 	 * Finalmente el método retorna el valor de sx > ss.
 	 * 
-	 * @param x de x
-	 * @param solucion de solucion
-	 * @param becas de fellowships
-	 * @return verdadero, si es mejor
+	 * 
+	 * @param x        Solución provisional al problema
+	 * @param solucion Solución del problema que podría dejar de serlo en este
+	 *                 momento
+	 * @param becas    Array con las becas almacenadas en el archivo .dat
+	 * @return verdadero Si la solución nueva es mejor que la anterior
 	 */
 	public static boolean esMejor(int[] x, int[] solucion, Beca[] becas) {
 		int sx = 0, ss = 0;
